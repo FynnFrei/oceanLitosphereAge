20c6c94b179fe8e98d5177293b1b1a5581b6ed37
FynnFrei <fynn.freist@gmail.com>
2025-01-22 14:46:49 +0100

    write and test downsampling functions

--- main.py
+++ main.py
@@ -1,59 +1,44 @@
 from scipy.io import netcdf_file
 import numpy as np
 from matplotlib import pyplot as plt
+from scripts import functions
 
 path = "grids/"
 age_file = netcdf_file(path + 'age.2020.1.GTS2012.6m.grd', 'r', mmap=False)
-elevation_file = netcdf_file(path + 'GRIDONE_2D_2008_1m.nc', mmap=False)
-
-
-def compress_grid(grid, block_size):
-    compressed_grid = np.zeros((grid.shape[0]//block_size, grid.shape[1]//block_size))
-    compressed_lat = np.linspace(-90, 90, grid.shape[0]//block_size)
-    compressed_lon = np.linspace(-180, 180, grid.shape[1]//block_size)
-    print(np.shape(compressed_grid))
-    for i in range(0, grid.shape[0]-1, block_size):     # calculate blocks
-        for j in range(0, grid.shape[1]-1, block_size):
-            compressed_grid[i//block_size, j//block_size] = (grid[i, j] + grid[i, j + 1] + grid[i + 1, j] + grid[i + 1, j + 1]) / 4
-
-    return compressed_lon, compressed_lat, compressed_grid
-
+bathymetry_file = netcdf_file(path + 'GRIDONE_2D_2008_1m.nc', mmap=False)
+# print(age_file.variables.keys())
+# print(bathymetry_file.variables.keys())
 
 # dataset a -> age_file
 a_lon_var = age_file.variables['lon']
 a_lat_var = age_file.variables['lat']
 a_grid_var = age_file.variables['z']
-a_lon = a_lon_var[:]
+a_lon = a_lon_var[:]        # reads longitude values into list
 a_lat = a_lat_var[:]
 a_grid = a_grid_var[:]      # age in Ma
 
-# dataset e -> elevation_file
-e_lon_var = elevation_file.variables['lon']
-e_lat_var = elevation_file.variables['lat']
-e_grid_var = elevation_file.variables['elevation']
-e_lon = e_lon_var[:]
-e_lat = e_lat_var[:]
-e_grid = e_grid_var[:]      # depth in m
+# dataset b -> bathymetry_file
+b_lon_var = bathymetry_file.variables['lon']
+b_lat_var = bathymetry_file.variables['lat']
+b_grid_var = bathymetry_file.variables['elevation']
+b_lon = b_lon_var[:]
+b_lat = b_lat_var[:]
+b_grid = b_grid_var[:]      # depth in m
 
-#print(age_file.variables.keys())
-#print(elevation_file.variables.keys())
 age_file.close()
-elevation_file.close()
+bathymetry_file.close()
 
-new_e_lon, new_e_lat, new_e_grid = compress_grid(e_grid, 12)
-new_a_lon, new_a_lat, new_a_grid = compress_grid(a_grid, 1)
-
-#print(np.shape(new_e_grid), np.shape(new_a_grid))
+# downsample grids:
+new_b_lon, new_b_lat, new_b_grid = functions.compress_grid(b_grid, 20)
+new_a_lon, new_a_lat, new_a_grid = functions.compress_grid(a_grid, 1)
 
 # create subplots:
 fig, axes = plt.subplots(2, 1, figsize=(12, 12))
 
-axes[0].pcolormesh(new_e_lon, new_e_lat, new_e_grid)
+axes[0].pcolormesh(new_b_lon, new_b_lat, new_b_grid)
 axes[0]. grid()
 
 axes[1].pcolormesh(new_a_lon, new_a_lat, new_a_grid)
 axes[1]. grid()
 
 plt.show()
-
-# this is test commit.
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,3 +1,37 @@
+import numpy as np
+
+
 # the theory of cooling oceanic litosphere in geo3 script1 P. 124
 
 # i need a reference depth at specific age for the sqrt(t) relation
+
+
+# the not ideal way to downsample data. Lot of processing.
+def compress_grid(grid, block_size):
+    compressed_grid = np.zeros((grid.shape[0] // block_size, grid.shape[1] // block_size))
+    compressed_lat = np.linspace(-90, 90, grid.shape[0] // block_size)
+    compressed_lon = np.linspace(-180, 180, grid.shape[1] // block_size)
+    print(f'Shape of compressed grid: {np.shape(compressed_grid)}')
+    for i in range(0, grid.shape[0] - 1, block_size):  # calculate blocks
+        for j in range(0, grid.shape[1] - 1, block_size):
+            # fill compressed_grid with values
+            compressed_grid[i // block_size, j // block_size] = grid[i:i + block_size, j:j + block_size].mean()
+
+    return compressed_lon, compressed_lat, compressed_grid
+
+
+# Does not work properly right now
+def downsample_grid(grid, block_size):
+    y, x = grid.shape
+    new_y = (y // block_size) * block_size
+    new_x = (x // block_size) * block_size
+    trimmed_grid = grid[:new_y, :new_x] # trimmed grid to match the divisible size
+    downsampled_lat = np.linspace(-90, 90, new_y // block_size)
+    downsampled_lon = np.linspace(-180, 180, new_x // block_size)
+
+    # Reshape into 4 dimensions-> each new pixel contains a 2D block -> 4D
+    reshaped = trimmed_grid.reshape(new_x // block_size, block_size, new_y // block_size, block_size)
+    downsampled_grid = reshaped.mean(axis=(1, 3))  # Average across block rows and block columns
+    print(np.shape(downsampled_grid))
+
+    return downsampled_lat, downsampled_lon, downsampled_grid
