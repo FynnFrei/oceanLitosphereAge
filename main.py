correct formula, plots

main.py
@@ -1,3 +1,5 @@
+from operator import is_not
+
 from scipy.io import netcdf_file
 import numpy as np
 from matplotlib import pyplot as plt
@@ -5,8 +7,9 @@ from matplotlib.colors import Normalize, ListedColormap
 from scripts import functions
 
 path = "grids/"
-age_file = netcdf_file(path + 'age.2020.1.GTS2012.2m.grd', 'r', mmap=False)
-bathymetry_file = netcdf_file(path + 'GRIDONE_2D_2008_2m.nc', mmap=False)
+accuracy = 2    # how many minutes° are one gridpoint
+age_file = netcdf_file(path + f'age.2020.1.GTS2012.{accuracy}m.grd', 'r', mmap=False)
+bathymetry_file = netcdf_file(path + f'GRIDONE_2D_2008_{accuracy}m.nc', mmap=False)
 #print(age_file.variables.keys())
 #print(bathymetry_file.variables.keys())
 
@@ -41,23 +44,23 @@ a_lat_trim, a_lon_trim_shift, a_grid_trim_shift = functions.trim_grid(a_lat, a_l
 b_grid_shift = functions.exclude_continental_plate(a_grid_trim_shift, b_grid_shift)     # make continents NaN
 
 # calculate difference:
-d_grid = functions.age_to_depth(a_grid_trim_shift)
-depth_anomaly = d_grid - b_grid_shift  # array shows how much calculated depth deviates from actual depth
+avr_ridge_depth = functions.calc_ridge_depth(a_grid_trim_shift, b_grid_shift, 0.01)
+d_grid = functions.age_to_depth(a_grid_trim_shift, avr_ridge_depth)
+depth_anomaly = abs(d_grid) - abs(b_grid_shift)  # array shows how much calculated depth deviates from actual depth
 
 # create subplots:
 
 cmap1 = plt.cm.viridis
 cmap2 = plt.cm.turbo_r
-cmap3 = plt.cm.seismic
+cmap3 = plt.cm.seismic_r
 continent_color = (0.5, 0.5, 0.5)
 cmap1.set_bad(color=continent_color)
 cmap2.set_bad(color=continent_color)
 cmap3.set_bad(color=continent_color)
 
 '''
-fig1, ax = plt.subplots(2, 2, figsize=(18, 8))
+fig1, ax = plt.subplots(2, 2, figsize=(22, 10))
 
-#mesh1 = ax[0][0].pcolormesh(a_lon_shift, a_lat, a_grid_shift, cmap=cmap2, vmax=200)
 mesh1 = ax[0][0].imshow(
     a_grid_shift,
     extent=[a_lon_shift.min(), a_lon_shift.max(), a_lat.min(), a_lat.max()],
@@ -66,9 +69,10 @@ mesh1 = ax[0][0].imshow(
     vmax=200,
     aspect=1
 )
-ax[0][0].set_title("Age in Ma")
+ax[0][0].set_title("Age of oceanic lithosphere")
+ax[0][0].text(-170, -85, "Ogg 2012")
 ax[0][0].set_aspect(1)
-fig1.colorbar(mesh1, ax=ax[0][0], label="Age")
+fig1.colorbar(mesh1, ax=ax[0][0], label="Age in Ma")
 ax[0][0].grid()
 
 mesh2 = ax[0][1].imshow(
@@ -78,19 +82,21 @@ mesh2 = ax[0][1].imshow(
     cmap=cmap1, vmin=-7000, vmax=0, aspect=1
 )
 ax[0][1].set_title("Bathymetric map")
+ax[0][1].text(-170, -85, "GEBCO_2020 Grid")
 ax[0][1].set_aspect(1)
-fig1.colorbar(mesh2, ax=ax[0][1], label="Depth")
+fig1.colorbar(mesh2, ax=ax[0][1], label="Depth in m")
 ax[0][1].grid()
 
 mesh3 = ax[1][0].imshow(
     d_grid,
     extent=[b_lon_shift.min(), b_lon_shift.max(), b_lat.min(), b_lat.max()],
     origin="lower",
-    cmap=cmap1, vmin=-7000, vmax=0, aspect=1
+    cmap=cmap1, vmin=-7000, vmax=-1000, aspect=1
 )
 ax[1][0].set_title("Calculated depth")
+ax[1][0].text(-170, -85, "Ogg 2012")
 ax[1][0].set_aspect(1)
-fig1.colorbar(mesh3, ax=ax[1][0], label="Depth")
+fig1.colorbar(mesh3, ax=ax[1][0], label="Depth in m")
 ax[1][0].grid()
 
 mesh4 = ax[1][1].imshow(
@@ -99,48 +105,58 @@ mesh4 = ax[1][1].imshow(
     origin="lower",
     cmap=cmap3, vmin=-3000, vmax=3000, aspect=1
 )
-ax[1][1].set_title("Depth anomaly")
+ax[1][1].set_title("Calculated depth - bathymetric depth")
+ax[1][1].text(-170, -85, "GEBCO_2020 Grid, Ogg 2012")
 ax[1][1].set_aspect(1)
-fig1.colorbar(mesh4, ax=ax[1][1], label="Δ Depth")
+fig1.colorbar(mesh4, ax=ax[1][1], label="Δ Depth in m")
 ax[1][1].grid()
 
 plt.show()
 '''
 
-
-fig2 = plt.figure(figsize=(16, 8))
+'''
+fig2 = plt.figure(figsize=(22, 10))
 plt.imshow(
     depth_anomaly,
-    extent=[b_lon_shift.min(), b_lon_shift.max(), b_lat.min(), b_lat.max()],
+    extent=[a_lon_shift.min(), a_lon_shift.max(), a_lat.min(), a_lat.max()],
     origin="lower",
-    cmap=cmap3, vmin=-3000, vmax=3000, aspect=1
+    cmap=cmap3,
+    vmin=-3000,
+    vmax=3000,
+    aspect=1
 )
-plt.title("Depth Anomaly (comparing calculated with real depth)")
-plt.colorbar(label="Δ Depth")
+plt.title("Calculated Depth - Bathymetric Depth")
+plt.text(-170, -85, "GEBCO_2020 Grid, Ogg 1012")
+plt.colorbar(label="Δ depth (in meters)")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
 plt.grid()
 plt.show()
+'''
 
 # functions.plot_scatter(d_grid, b_grid_shift)
-'''
+
 grid1_flat = np.reshape(d_grid, -1)
 grid2_flat = np.reshape(b_grid_shift, -1)
 mask_nan = ~np.isnan(grid1_flat) & ~np.isnan(grid2_flat)
 grid1_flat_clean = grid1_flat[mask_nan]
 grid2_flat_clean = grid2_flat[mask_nan]
-ratio = grid2_flat_clean / grid1_flat_clean
+# ratio = grid2_flat_clean / grid1_flat_clean
 diff = grid2_flat_clean - grid1_flat_clean
-
-fig4 = plt.figure()
-plt.axvline(x=1, alpha=0.4, color='red', linestyle='--', linewidth=2)
-plt.hist(ratio, bins=120, alpha=1, range=(-1, 5))
-plt.title("Distribution of real/calc depth")
-plt.show()
 
 fig5 = plt.figure()
 plt.axvline(x=0, alpha=0.4, color='red', linestyle='--', linewidth=2)
-plt.hist(diff, bins=100, alpha=1)
+plt.hist(diff, bins=100, alpha=1, range=(-3000, 7000))
 plt.title("Distribution of Δ depth")
-plt.text(-6000, 2*10**5, "Deeper than calculation")
-plt.text(2000, 2*10**5, "Shallower than calculation")
+plt.text(5800, 1.95*10**6, "GEBCO_2020 Grid, Ogg 1012")
+plt.xlabel("anomaly in m")
+plt.ylabel("amount of data points")
+plt.text(-3000, 8*10**5, "Deeper than calculation", color="red")
+plt.text(2000, 8*10**5, "Shallower than calculation", color="blue")
 plt.show()
-'''
+
+'''fig4 = plt.figure()
+plt.axvline(x=1, alpha=0.4, color='red', linestyle='--', linewidth=2)
+plt.hist(ratio, bins=120, alpha=1, range=(-1, 5))
+plt.title("Distribution of real/calc depth")
+plt.show()'''

scripts/functions.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.io import netcdf_file
 import os
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 
 
 # the theory of cooling oceanic litosphere in geo3 script1 P. 124
@@ -83,18 +84,29 @@ def write_to_netcdf(output_filename, lat, lon, grid):
     print(f"NetCDF file '{full_output_path}' created successfully.")
 
 
-def age_to_depth(grid):
-    '''m = -0.3
-    depth_grid = m * np.sqrt(1000000 * grid)'''
+def calc_ridge_depth(a_grid, b_grid, max_age):
+    # calculates the mean depth for a given max crust age
+    a_grid_ridge = np.where(a_grid > max_age, np.nan, a_grid)
+    ridge_depth = np.where(a_grid_ridge > 0, b_grid, np.nan)
+    avr_ridge_depth = np.nanmean(ridge_depth)   # calculate mean depth
+    # num_of_points = np.sum(a_grid_ridge > 0)
+    # print(avr_ridge_depth, num_of_points)
 
-    alpha = 3 * 10 ** -5  # coefficient of thermal expansion in 1/K
+    return avr_ridge_depth
+
+
+def age_to_depth(grid, d_0):
+
+    alpha = 2 * 10 ** -5  # coefficient of thermal expansion in 1/K
     T_m = 1570  # mantle temperature in K (e.g. Vl.1, P.115)
     rho_m = 3300  # initial density of the hot mantle at T_m in Kg/m^3 (example value)
-    rho_w = 1000  # density of water in kg/m^3
+    rho_w = 1030  # density of water in kg/m^3
     kappa = 10 ** -6  # thermal diffusivity in m^2/s (data from Vl.1, P. 115)
     t = 3600 * 24 * 365 * 1000000 * grid  # time in s
+    # d_0 is average ocean ridge depth
+    # d is not absolute depth but depth change compared to ocean ridge
 
-    d = -(2 * alpha * rho_m * T_m / (rho_m - rho_w)) * np.sqrt((kappa / np.pi) * t)
+    d = -(2 * alpha * rho_m * T_m / (rho_m - rho_w)) * np.sqrt((kappa / np.pi) * t) + d_0
     return d
 
 
@@ -154,14 +166,23 @@ def plot_scatter(grid1, grid2):
     grid2_flat_clean = grid2_flat[mask_nan]
     mask_positive = grid2_flat_clean > 0
 
-    optimum_line = np.linspace(-10000, 0, 2)
+    optimum_line = np.linspace(-8000, -2000, 2)
 
-    fig3 = plt.figure(figsize=(16, 12))
-    plt.scatter(grid1_flat_clean[~mask_positive], grid2_flat_clean[~mask_positive], s=0.01, color="blue")
-    plt.scatter(grid1_flat_clean[mask_positive], grid2_flat_clean[mask_positive], s=0.01, color="green")    # above sea level
+    fig3 = plt.figure(figsize=(22, 12))
+    plt.scatter(grid1_flat_clean[~mask_positive], grid2_flat_clean[~mask_positive], s=0.0001, color="blue")
+    plt.scatter(grid1_flat_clean[mask_positive], grid2_flat_clean[mask_positive], s=0.0001, color="green")    # above sea level
     plt.plot(optimum_line, optimum_line, color="red")
     plt.gca().invert_xaxis()
-    plt.title("real depth over calculated depth")
+    plt.title("Real Depth Over Calculated Depth (low detail)")
+    plt.text(-2000, -10000, "GEBCO_2020 Grid, Ogg 2012")
     plt.xlabel("calculated depth")
     plt.ylabel("real depth")
+    # Custom legend markers for larger dots
+    legend_elements = [
+        Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10, label="Below Sea Level"),
+        Line2D([0], [0], marker='o', color='w', markerfacecolor='green', markersize=10, label="Above Sea Level"),
+        Line2D([0], [0], color='red', linewidth=2, label="calc. depth = real depth")
+    ]
+    plt.legend(handles=legend_elements, loc="upper right")
     plt.show()
+